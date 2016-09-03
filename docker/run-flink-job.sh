f5fb7ebaaca085ce9549dca0d06000f55de210d8
Damian Zientalak <damian.zientalak@gmail.com>
2016-09-04 00:24:00 +0200

    Fixes.

--- docker/run-flink-job.sh
+++ docker/run-flink-job.sh
@@ -2,10 +2,10 @@
 
 docker exec -it jobmanager ./bin/flink run -d -p 2 \
 -c io.weba.processor.flink.job.IngestEventsElasticsearchJob weba.jar \
---kafka-bootstrap-servers 100.0.0.205:9092 --kafka-group-id IngestEventsElasticsearchJob \
+--kafka-bootstrap-servers ns381473.ip-94-23-248.eu:9092 --kafka-group-id IngestEventsElasticsearchJob \
 --kafka-topic eventor_webserver \
 --es-index webaio_events \
 --es-type webaio_events \
---es-servers 100.0.0.205:9300 \
+--es-servers ns381473.ip-94-23-248.eu:9300 \
 --es-cluster-name webaio \
 -Dorg.xerial.snappy.tempdir=/tmp/snappy
