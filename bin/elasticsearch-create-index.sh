5844c7bc70b923d06d5a171781d11fcea9f0b9a6
Damian Zientalak <damian.zientalak@gmail.com>
2016-09-16 20:17:06 +0200

    Add bin utils.

---
+++ bin/elasticsearch-create-index.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -H 'Content-Type: application/json' -X PUT -d @../docker/elasticsearch/mapping/weba_events.json http://100.0.0.1:9200/weba_events
---
+++ bin/elasticsearch-recreate-index.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./elasticsearch-delete-index.sh
+./elasticsearch-create-index.sh
